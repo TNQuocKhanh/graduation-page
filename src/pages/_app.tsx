b0f11bd34b32d53fcb58ba2a8a41b693c7918f4c
Khanh Tran <khanh.tran@nexpando.com>
2023-12-14 21:40:39 +0700

    add seo img

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,39 @@
-import React, { FC } from 'react'
-import Head from 'next/head'
-import type { AppProps } from 'next/app'
-import { CssBaseline } from '@mui/material'
-import { EmotionCache } from '@emotion/cache'
-import { CacheProvider } from '@emotion/react'
-import { createEmotionCache } from '@/utils'
-import { MUIProvider } from '@/providers'
-import 'slick-carousel/slick/slick.css'
-import '@/styles/globals.css'
-import '@/styles/react-slick.css'
-import { NextPageWithLayout } from '@/interfaces/layout'
+import React, { FC } from "react";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { CssBaseline } from "@mui/material";
+import { EmotionCache } from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { createEmotionCache } from "@/utils";
+import { MUIProvider } from "@/providers";
+import "slick-carousel/slick/slick.css";
+import "@/styles/globals.css";
+import "@/styles/react-slick.css";
+import { NextPageWithLayout } from "@/interfaces/layout";
 // import 'slick-carousel/slick/slick-theme.css'
 
 // Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache()
+const clientSideEmotionCache = createEmotionCache();
 
 type AppPropsWithLayout = AppProps & {
-  emotionCache: EmotionCache
-  Component: NextPageWithLayout
-}
+  emotionCache: EmotionCache;
+  Component: NextPageWithLayout;
+};
 
 const App: FC<AppPropsWithLayout> = (props: AppPropsWithLayout) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || ((page) => page);
 
   return (
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/TNQuocKhanh/graduation-page/main/public/images/graduation-bg.png"
+        />
         <title>My graduation</title>
       </Head>
       <MUIProvider>
@@ -39,7 +43,7 @@ const App: FC<AppPropsWithLayout> = (props: AppPropsWithLayout) => {
         {getLayout(<Component {...pageProps} />)}
       </MUIProvider>
     </CacheProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
